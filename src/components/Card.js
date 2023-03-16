6650de7db03986efed0d26140f2e0f31208e0d2c
Aram Hekimian <aramhek121@gmail.com>
2023-03-15 19:45:08 -0600

    implement horizontal cards style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,21 +2,17 @@ import React from "react";
 import star from "/Users/aramhekimian/repos/airbnb-experiences/src/assets/star.png";
 export default function Card(props) {
   return (
-    <div className="card--cont">
-      <img className="card--img" src={`./images/${props.img}`} alt="" />
-      <div className="card--reviews">
-        <img className="card--star" src={star} alt="star icon" />
-        <span className="card--score">
-          {props.rating}
-          <span className="gray">
-            ({props.reviewCount}) • {props.location}
-          </span>
-        </span>
+    <div className="card">
+      <img src={`../images/${props.img}`} className="card--image" />
+      <div className="card--stats">
+        <img src={star} className="card--star" />
+        <span>{props.rating}</span>
+        <span className="gray">({props.reviewCount}) • </span>
+        <span className="gray">{props.location}</span>
       </div>
       <p className="card--title">{props.title}</p>
-
-      <p className="card--pricing">
-        <span className="card--pricing-bold">From ${props.price}</span> / person
+      <p className="card--price">
+        <span className="bold">From ${props.price}</span> / person
       </p>
     </div>
   );
